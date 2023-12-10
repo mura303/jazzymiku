93d2da6e085627c2b2ba7f523e956da7394945d7
DN <dn@example.com>
2023-12-10 22:33:07 +0900

    delete unused codes

--- find_tempo.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-import sys
-import pathlib
-import wave
-import librosa
-
-y, sr = librosa.load(sys.argv[1])
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))

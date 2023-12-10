6a61b26d619fb1f3efbe7da9dca14ff81a6035d0
DN <dn@example.com>
2023-12-10 22:28:59 +0900

    delete test files

--- solfe_each_measure.py
+++ solfe_each_measure.py
@@ -3,14 +3,29 @@ from common import cromatic
 import pathlib
 import sys
 import argparse
+from subprocess import run
+import tempfile  # Added this line
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--key', '-k', help='key')
-parser.add_argument('--input', '-i', help='input file')
-parser.add_argument('--output', '-o', help='output file')
-args = parser.parse_args()
+parser.add_argument('infile', help='infile')
+parser.add_argument('outdir', help='outdir')
 
-score = converter.parse(args.input)
+args = parser.parse_args()
+infilep = pathlib.Path(args.infile)
+
+score = converter.parse(infilep)
+
+key = score.analyze('key')
+print(f"Key of the song: {key}")
+
+num_sharps_flats = key.sharps
+print(f"Number of sharps or flats: {num_sharps_flats}")
+
+key_diff = -num_sharps_flats
+
+def trim_silence( wavfile ):
+    tmpdir = pathlib.Path(tempfile.gettempdir())
+    tmpfile = tmpdir / 'tmpfile.wav'
 
 cantus = score.parts[0]
 
@@ -18,7 +33,7 @@ for i, measure in enumerate(cantus.getElementsByClass(stream.Measure), start=1):
     new_score = stream.Score()
     for n in measure.notesAndRests:
         if isinstance(n, note.Note):
-            new_note = note.Note( pitch=60+int(args.key), 
+            new_note = note.Note( pitch=60+int(key_diff), 
                                         quarterLength=n.quarterLength,
                                         lyric=n.lyric,
                                         )
@@ -35,4 +50,5 @@ for i, measure in enumerate(cantus.getElementsByClass(stream.Measure), start=1):
     new_score[-1].tie = None
 
     if len(new_score.getElementsByClass(note.Note)) > 0:
-        new_score.write('xml', fp=f"{args.output}{i}.musicxml")
+        outdirp = pathlib.Path(args.outdir)
+        new_score.write('xml', fp=f"{outdirp/infilep.stem}{i}.musicxml")

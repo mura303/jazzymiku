24f8fdf2509172a6dd4acc51a52c1edd5ae41e96
DN <dn@example.com>
2024-02-27 23:25:59 +0900

    メロディも入れる

--- biab_bassline.py
+++ biab_bassline.py
@@ -20,7 +20,9 @@ else:
     measure_par_chorus = 32
 
 bass_in = score.parts[0]
+melody_in = score.parts[-1]  # 最後のパートがメロディーと仮定
 print(f"bass_part: {bass_in.partName}")
+print(f"melody_part: {melody_in.partName}")
 
 key = score.analyze('key')
 print(f"analyzed key of the song: {key}")
@@ -32,15 +34,15 @@ else:
 
 print(f"key diff: {key_diff}")
 
-
 beats_per_measure = bass_in.getTimeSignatures()[0].numerator
 
 bass_out = stream.Score()
+melody_out = stream.Score()
 
 tempo = bass_in.metronomeMarkBoundaries()[0][-1]
 bass_out.insert(0, tempo)
+melody_out.insert(0, tempo)
 last_pitch = None # おかしな楽譜だとありえるので例外が出るようにしておく
-
 
 for i, measure in enumerate(bass_in.getElementsByClass(stream.Measure)):
     if i == 2+measure_par_chorus:
@@ -59,5 +61,23 @@ for i, measure in enumerate(bass_in.getElementsByClass(stream.Measure)):
         elif isinstance(n, note.Rest):
             bass_out.append(note.Rest(quarterLength=n.quarterLength))
 
+for i, measure in enumerate(melody_in.getElementsByClass(stream.Measure)):
+    if i < 2:
+        continue
+    if i == 2+measure_par_chorus:
+        break
+    for n in measure.notesAndRests:
+        if isinstance(n, note.Note):
+            if n.tie and n.tie.type in ["continue", "stop"]:
+                lyric = 'ー'
+            else:
+                lyric = cromatic[(n.pitch.midi + key_diff) % 12]
+            new_note = note.Note(pitch=n.pitch.midi,
+                                 quarterLength=n.quarterLength,
+                                 lyric=lyric)
+            new_note.tie = n.tie
+            bass_out.append(new_note)
+        elif isinstance(n, note.Rest):
+            bass_out.append(note.Rest(quarterLength=n.quarterLength))
 
 bass_out.write('xml', fp=args.outfile)
